b6f3a88f61f58008190b82badec25f1df1cd4e90
Aletavrus <152197218+Aletavrus@users.noreply.github.com>
2024-01-01 23:24:47 +0300

    Heap_Sort Test

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Sorting
+using System;
+
+namespace Sorting
 {
     internal class Program
     {
@@ -29,6 +31,14 @@
             {
                 array = Bubble_Sort(array);
             }
+            if (type == "quick")
+            {
+                array = Quick_Sort(array);
+            }
+            if (type=="heap")
+            {
+                array = Heap_Sort(array, true);
+            }
 
             Print(array);
         }
@@ -44,6 +54,7 @@
         }
         public static int[] CreateArray(int size)
         {
+            Console.WriteLine("Enter a value (1 per string)");
             int[] array = new int[size];
             for (int i = 0; i < size; i++)
             {
@@ -100,6 +111,169 @@
                 }
             }
             return array;
+        }
+
+        public static int[] Quick_Sort(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return array;
+            }
+            int pivot = array[array.Length-1];
+            int swap_marker = -1;
+
+            if (array.Length == 1)
+            {
+                return array;
+            }
+
+            if (array.Length == 2)
+            {
+                if (array[0] > array[1])
+                {
+                    int temp_el = array[0];
+                    array[0] = array[1];
+                    array[1] = temp_el;
+                }
+                return array;
+            }
+
+            for (int index=0; index< array.Length; index++)
+            {
+                if (array[index] <= pivot)
+                {
+                    swap_marker++;
+                    if (swap_marker < index)
+                    {
+                        int temp_el = array[swap_marker];
+                        array[swap_marker] = array[index];
+                        array[index] = temp_el;
+                    }
+                }
+            }
+
+            int size_big = 0;
+            int size_small = 0;
+
+            for (int i=0; i< array.Length; i++)
+            {
+                if (array[i] > pivot)
+                {
+                    size_big++;
+                }
+                if (array[i] < pivot)
+                {
+                    size_small++;
+                }
+            }
+
+            int[] small_array = new int[size_small];
+            int[] big_array = new int[size_big];
+
+            int index_big = 0;
+            int index_small = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > pivot)
+                {
+                    big_array[index_big] = array[i];
+                    index_big++;
+                }
+                if (array[i] < pivot)
+                {
+                    small_array[index_small] = array[i];
+                    index_small++;
+                }
+            }
+            int[] new_small = Quick_Sort(small_array);
+
+            int[] new_big = Quick_Sort (big_array);
+
+            int[] pivot_element = { pivot };
+            int[] result = new_small.Concat(pivot_element.Concat(new_big)).ToArray();
+            return result;
+        }
+        
+        public static int[] Heap_Sort(int[] array, bool first)
+        {
+            for (int i=1; i<array.Length; i+=2)
+            {
+                if (array[i]>0 & array[i]<array[i+1])
+                {
+                    int temp_el = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp_el;
+                }
+            }
+            for (int i=1; i< array.Length; i+=2)
+            {
+                if (i%2!=0)
+                {
+                    if (array[i]>array[i-1])
+                    {
+                        int temp_el = array[i];
+                        array[i] = array[i-1];
+                        array[i-1] = temp_el;
+                    }
+                }
+                else
+                {
+                    if (array[i]>array[i-2])
+                    {
+                        int temp_el = array[i];
+                        array[i] = array[i-2];
+                        array[i-2] = temp_el;
+                    }
+                }
+            }
+            
+            int max = array[0];
+            int min = array[0];
+            
+            for (int i = 0; i<array.Length; i++)
+            {
+                if (min>array[i])
+                {
+                    min = array[i];
+                }
+            }
+            
+            int[] new_array = new int[array.Length-1];
+            array[0] = min;
+            int counter = 0;
+            
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i]!=max)
+                {
+                    new_array[counter] = array[i];
+                    counter++;
+                }
+            }
+            
+            int[] good_arr = new int[array.Length];
+            
+            if (!first)
+            {
+                return new_array;
+            }
+            else
+            {
+                for (int i=0; i<array.Length; i++)
+                {
+                    int index_paste = array.Length-i-1;
+                    good_arr[index_paste] = max;
+                    new_array = Heap_Sort(new_array, false);
+                    max = Find_Max(new_array);
+                }
+                return good_arr;
+            }
+        }
+        
+        public static int Find_Max(int[] array)
+        {
+            return array[0];
         }
     }
 }
