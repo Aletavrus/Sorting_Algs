d3163d8af4a10544f48a6b8bd2cd1198a91a2bd6
Aletavrus <152197218+Aletavrus@users.noreply.github.com>
2024-01-08 15:10:08 +0300

    Heap Sort DONE

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sorting
 {
@@ -14,13 +15,12 @@ namespace Sorting
             if (command == "yes")
             {
                 array = CreateRandomArray(size);
-                Print(array);
             }
             else
             {
                 array = CreateArray(size);
-                Print(array);
             }
+            Print(array);
             Console.WriteLine("Which type of sorting algorithm do you want to use?");
             string type = Console.ReadLine();
             if (type == "insertion")
@@ -35,18 +35,19 @@ namespace Sorting
             {
                 array = Quick_Sort(array);
             }
-            if (type=="heap")
+            if (type == "heap")
             {
                 array = Heap_Sort(array, true);
             }
 
             Print(array);
         }
-        public static int[] CreateRandomArray (int size)
+
+        public static int[] CreateRandomArray(int size)
         {
             Random random = new Random();
             int[] array = new int[size];
-            for (int i =0; i < size; i++)
+            for (int i = 0; i < size; i++)
             {
                 array[i] = random.Next(0, 1000);
             }
@@ -54,51 +55,65 @@ namespace Sorting
         }
         public static int[] CreateArray(int size)
         {
-            Console.WriteLine("Enter a value (1 per string)");
+            Console.WriteLine("Enter a list");
+            string input = Console.ReadLine();
+            string[] split_input = input.Split();
             int[] array = new int[size];
             for (int i = 0; i < size; i++)
             {
-                string input = Console.ReadLine();
-                int element = int.Parse(input);
-                array[i] = element;
+                array[i] = int.Parse(split_input[i]);
             }
             return array;
+
         }
         public static void Print(int[] array)
         {
-            for (int i = 0; i< array.Length;i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.Write($"{array[i]} ");
             }
             Console.WriteLine();
         }
-        
-        public static int[] Add_Two (int[] array_1, int[] array_2)
+
+        public static int[] Add_Two(int[] array_1, int[] array_2) //Connectind 2 arrays for quick sort
         {
             int size_1 = array_1.Length;
             int size_2 = array_2.Length;
             int big_size = array_1.Length + array_2.Length;
             int[] new_array = new int[big_size];
-            for (int i=0; i<size_1; i++)
+            for (int i = 0; i < size_1; i++)
             {
                 new_array[i] = array_1[i];
             }
-            for (int i=0; i<array_2.Length; i++)
+            for (int i = 0; i < array_2.Length; i++)
             {
-                new_array[size_1+i] = array_2[i];
+                new_array[size_1 + i] = array_2[i];
             }
             return new_array;
         }
 
+        public static int Find_Max(int[] array) //Find max element (for heap sort)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
         public static int[] Insertion_Sort(int[] array)
         {
-            for (int i = 1; i < array.Length;i++)
+            for (int i = 1; i < array.Length; i++)
             {
                 int index_of_el = i;
                 int j = i - 1;
                 int value_to_compare = array[j];
                 int swap_value = array[i];
-                while(swap_value < value_to_compare & j>=0)
+                while (swap_value < value_to_compare & j >= 0)
                 {
                     (array[index_of_el], array[j]) = (array[j], array[index_of_el]);
                     index_of_el = j;
@@ -115,15 +130,15 @@ namespace Sorting
 
         public static int[] Bubble_Sort(int[] array)
         {
-            for (int i = 0; i< array.Length-1; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 0;  j < array.Length-i-1; j++)
+                for (int j = 0; j < array.Length - i - 1; j++)
                 {
-                    if (array[j] > array[j+1])
+                    if (array[j] > array[j + 1])
                     {
                         int temp_el = array[j];
-                        array[j] = array[j+1];
-                        array[j+1] = temp_el;
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp_el;
                     }
                 }
             }
@@ -136,7 +151,7 @@ namespace Sorting
             {
                 return array;
             }
-            int pivot = array[array.Length-1];
+            int pivot = array[array.Length - 1];
             int swap_marker = -1;
 
             if (array.Length == 1)
@@ -155,7 +170,7 @@ namespace Sorting
                 return array;
             }
 
-            for (int index=0; index< array.Length; index++)
+            for (int index = 0; index < array.Length; index++)
             {
                 if (array[index] <= pivot)
                 {
@@ -172,7 +187,7 @@ namespace Sorting
             int size_big = 0;
             int size_small = 0;
 
-            for (int i=0; i< array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] > pivot)
                 {
@@ -205,79 +220,95 @@ namespace Sorting
             }
             int[] new_small = Quick_Sort(small_array);
 
-            int[] new_big = Quick_Sort (big_array);
+            int[] new_big = Quick_Sort(big_array);
 
             int[] pivot_element = { pivot };
             int[] result = Add_Two(new_small, Add_Two(pivot_element, new_big));
             return result;
         }
-        
+
         public static int[] Heap_Sort(int[] array, bool first)
         {
-            for (int i=1; i< array.Length; i++)
+            int max = Find_Max(array);
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(i);
-                if (i%2!=0)
+                if (2 * i + 1 < array.Length)
                 {
-                    if (array[i]>array[i-1])
+                    if (array[i] < array[2 * i + 1])
                     {
                         int temp_el = array[i];
-                        array[i] = array[i-1];
-                        array[i-1] = temp_el;
+                        array[i] = array[2 * i + 1];
+                        array[2 * i + 1] = temp_el;
                     }
                 }
-                else
+                if (2 * i + 2 < array.Length)
                 {
-                    if (array[i]>array[i-2])
+                    if (array[i] < array[2 * i + 2])
                     {
                         int temp_el = array[i];
-                        array[i] = array[i-2];
-                        array[i-2] = temp_el;
+                        array[i] = array[2 * i + 2];
+                        array[2 * i + 2] = temp_el;
                     }
                 }
             }
-            
-            int max = array[0];
-            int min = array[0];
-            
-            for (int i = 0; i<array.Length; i++)
+            while (max != array[0])
             {
-                if (min>array[i])
+                for (int i = 0; i < array.Length; i++)
                 {
-                    min = array[i];
+                    if (2 * i + 1 < array.Length)
+                    {
+                        if (array[i] < array[2 * i + 1])
+                        {
+                            int temp_el = array[i];
+                            array[i] = array[2 * i + 1];
+                            array[2 * i + 1] = temp_el;
+                        }
+                    }
+                    if (2 * i + 2 < array.Length)
+                    {
+                        if (array[i] < array[2 * i + 2])
+                        {
+                            int temp_el = array[i];
+                            array[i] = array[2 * i + 2];
+                            array[2 * i + 2] = temp_el;
+                        }
+                    }
                 }
             }
-            
-            int[] new_array = new int[array.Length-1];
-            array[0] = min;
+
+            max = array[0];
+
+            int[] new_array = new int[array.Length - 1];
             int counter = 0;
-            
-            for (int i=0; i<array.Length; i++)
+
+            for (int i = 0; i < array.Length; i++)
             {
-                if (array[i]!=max)
+                if (array[i] != max)
                 {
                     new_array[counter] = array[i];
                     counter++;
                 }
             }
-            
+
             int[] good_arr = new int[array.Length];
-            
+
+
             if (!first)
             {
                 return new_array;
             }
             else
             {
-                for (int i=0; i<array.Length; i++)
+                good_arr[array.Length-1] = array[0];
+                good_arr[array.Length - 2] = Find_Max(new_array);
+                for (int i = 2; i < array.Length; i++)
                 {
-                    int index_paste = array.Length-i-1;
-                    good_arr[index_paste] = max;
+                    int index_paste = array.Length - i - 1;
                     new_array = Heap_Sort(new_array, false);
-                    Console.WriteLine(new_array.Length);
-                    if (new_array.Length>0)
+                    if (new_array.Length > 0)
                     {
-                        max = new_array[0];
+                        max = Find_Max(new_array);
+                        good_arr[index_paste] = max;
                     }
                     else
                     {
